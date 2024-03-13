4df2ca7dd0eb818906d645de804a1af5d337e361
oshan35 <oshan.20191052@iit.ac.lk>
2024-03-13 16:31:14 +0530

    first commit

---
+++ src/test/java/com/example/FolkloreBackend/FolkloreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FolkloreBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolkloreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
